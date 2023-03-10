78e3626559292f01234d5dabd333c3f3ee8572d4
Cristian Cosenza <me@cristiancosenza.com>
2023-03-10 17:33:59 +0100

    feat: convert date to it_IT format feat: likes

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ const containerElement = document.getElementById('container');
 // elemento post da duplicare per rappresentare il post
 const templateElement = document.getElementById('template-post');
 
+let counter = 0;
 //struttura dati contenente la rappresentazione dei post
 const posts = [
     {
@@ -22,7 +23,7 @@ const posts = [
             age: 39,
             nPosts: 150
         },
-        created: '03-10-2023',
+        created: '3-10-2023',
         content: 'Lorem ipsum',
         media: 'https://unsplash.it/600/300?image=14',
         likes: 1
@@ -67,7 +68,7 @@ const posts = [
         likes: 80
     }
 ];
-
+let likedPosts = [];
 
 
 
@@ -85,18 +86,59 @@ function renderPost(elementHTML, postElement) {
     //author name
     elementHTML.querySelector('.post-meta__author').innerHTML = nameAuthor;
     //date
-    elementHTML.querySelector('.post-meta__time').innerHTML = created;
+    console.log('created', created)
+    const createdAt = convertToIt(created);
+    elementHTML.querySelector('.post-meta__time').innerHTML = createdAt;
     //description
     elementHTML.querySelector('.post__text').innerHTML = content;
     //image
     elementHTML.querySelector('.post__image > img').src = media;
     //likes
-    elementHTML.querySelector('.js-likes-counter').id = `like-counter-${id}`;
-    elementHTML.querySelector('.js-likes-counter').innerHTML = likes;
+    const elementLikeCounter = elementHTML.querySelector('.js-likes-counter');
+    elementLikeCounter.id = `like-counter-${id}`;
+    elementLikeCounter.innerHTML = likes;
+
+    const likeButton = elementHTML.querySelector('.like-button');
+    likeButton.addEventListener('click', function () {
+        if (likedPosts.includes(id)) {
+            likeButton.classList.remove('like-button--liked');
+
+            postElement.likes--;
+            console.log('likes', postElement.likes);
+
+            console.log('elementLikeCounter', elementLikeCounter)
+            elementLikeCounter.innerHTML = postElement.likes;
+
+            likedPosts = likedPosts.filter(element => element !== id);
+            console.log('likedPosts', likedPosts)
+            return;
+        }
+
+        likeButton.classList.add('like-button--liked');
+
+        postElement.likes++;
+        console.log('likes', postElement.likes);
+
+        console.log('elementLikeCounter', elementLikeCounter)
+        elementLikeCounter.innerHTML = postElement.likes;
+
+        likedPosts.push(id);
+    });
 
     return elementHTML;
 }
 
+// 10-03-2023
+
+// Con questa funzione convertiamo una data in qualsiasi formato in una data italiana
+function convertToIt(created) {
+    const dates = created.split('-');
+    console.log('dates', dates);
+    const giorno = dates[1].padStart(2, '0');
+    const mese = dates[0].padStart(2, '0');
+    const anno = dates[2];
+    return `${giorno}-${mese}-${anno}`;
+}
 
 //Realizzare la lista dei post - idee e ipotesi
 //1. creo un ciclo che attraversa la lista dei post
